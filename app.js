60ac0d825138e9a220379ef2faf12945f1f4eb1e
Tanner Leigh <tannerhleigh@gmail.com>
2020-07-15 14:03:50 -0700

    [arrays] Add favorite teams

---
+++ app.js
@@ -0,0 +1,15 @@
+// string
+var name = "Tanner";
+console.log(name);
+
+var myLocation = "Los Angeles";
+console.log(myLocation);
+
+// number
+var favoriteNumber = 5;
+console.log(favoriteNumber);
+
+// array
+var teams = ["Rockets", "Lakers", "Warrios", "Clippers", "Blazers", "Celtics", "Nets"];
+console.log(teams);
+
