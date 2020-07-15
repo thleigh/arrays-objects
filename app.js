d88922d0af071d6d640b7c59cb74dc97d0dd926d
Tanner Leigh <tannerhleigh@gmail.com>
2020-07-15 14:26:28 -0700

    [arrays] add while loops

--- app.js
+++ app.js
@@ -13,3 +13,24 @@ console.log(favoriteNumber);
 var teams = ["Rockets", "Lakers", "Warrios", "Clippers", "Blazers", "Celtics", "Nets"];
 console.log(teams);
 
+// while loops
+var number = 10;
+var otherNumber = 1;
+while (otherNumber < number) {
+    console.log(otherNumber);
+    otherNumber += 1;
+}
+
+ var numberTwo = 20;
+ var myOtherNumber = 10;
+ while (myOtherNumber < numberTwo) {
+     console.log(myOtherNumber);
+     myOtherNumber += 1;
+}
+
+var numberThree = 30;
+var myOtherNumberTwo = 20;
+while (myOtherNumberTwo < numberThree) {
+    console.log(myOtherNumberTwo);
+    myOtherNumberTwo += 1;
+}
